9de5567e74457d613c516abf61f3cbe5cff1dcf0
mabo <mabo.daniel@gmail.com>
2019-03-10 15:37:51 -0600

    Modificado el espaciado

--- client.c
+++ client.c
@@ -8,47 +8,45 @@
    
 int main(int argc, char const *argv[]) 
 { 
-    struct sockaddr_in address; 
-    int sock = 0, valread; 
-    struct sockaddr_in serv_addr; 
-    char *hello = "Hello from client"; 
-    char buffer[1024] = {0}; 
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
-    { 
-        printf("\n Socket creation error \n"); 
-        return -1; 
-    } 
+  struct sockaddr_in address; 
+  int sock = 0, valread; 
+  struct sockaddr_in serv_addr; 
+  char *hello = "Hello from client"; 
+  char buffer[1024] = {0}; 
+  if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) { 
+    printf("\n Socket creation error \n");
+    return -1;
+  }
+  
+  memset(&serv_addr, '0', sizeof(serv_addr)); 
    
-    memset(&serv_addr, '0', sizeof(serv_addr)); 
+  serv_addr.sin_family = AF_INET; 
+  serv_addr.sin_port = htons(PORT); 
+     
+  // Convert IPv4 and IPv6 addresses from text to binary form 
+  if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) { 
+    printf("\nInvalid address/ Address not supported \n"); 
+    return -1; 
+  } 
    
-    serv_addr.sin_family = AF_INET; 
-    serv_addr.sin_port = htons(PORT); 
-       
-    // Convert IPv4 and IPv6 addresses from text to binary form 
-    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)  
-    { 
-        printf("\nInvalid address/ Address not supported \n"); 
-        return -1; 
-    } 
-   
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
-    { 
-        printf("\nConnection Failed \n"); 
-        return -1; 
-    }
+  if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) { 
+    printf("\nConnection Failed \n"); 
+    return -1; 
+  }
 
-    valread = read( sock , buffer, 1024); 
-    printf("%s\n",buffer);
+  valread = read( sock , buffer, 1024); 
+  printf("%s\n",buffer);
     
-    for(;;){
-      scanf("%s",buffer);
-      do{
-        printf("to:sent:\t%s\n", buffer);
-        send(sock,buffer,strlen(buffer),0);
-        valread = read(sock,buffer,1024);
-        printf("recived::\t\t%s\n", buffer);
-      }while(scanf("%126[^\n]",buffer));
-    }
+  for(;;){
+    scanf("%s",buffer);
+    if(buffer == "quit"){break;}
+    do{
+      printf("to:sent:\t%s\n", buffer);
+      send(sock,buffer,strlen(buffer),0);
+      valread = read(sock,buffer,1024);
+      printf("recived::\t\t%s\n", buffer);
+    } while(scanf("%126[^\n]",buffer));
+  }
 
-    return 0; 
+  return 0; 
 } 
