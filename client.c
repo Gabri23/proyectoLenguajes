9621692a9c77b8ee423ff9ac19fa6dd90ea63b07
mabo <mabo.daniel@gmail.com>
2019-03-15 10:49:55 -0600

    Recepción de port, server_ip y server_port de terminal. Uso: client s_ip s_port c_port

--- client.c
+++ client.c
@@ -4,16 +4,16 @@
 #include <stdlib.h> 
 #include <netinet/in.h> 
 #include <string.h> 
-//#define PORT 8888 
-   
+
 int main(int argc, char const *argv[]) 
 {
-  int PORT = 0;
+  int PORT = 0, mPORT = 12020;
   char *ip_serv = "";
   if(argc < 3){
-    printf("usge: client ip-ser port-ser\n");
+    printf("usge: %s ip-ser port-ser port-cli\n",argv[0]);
     return -1;
   } else {
+    mPORT = atoi(3[argv]);
     PORT = atoi(2[argv]);
     ip_serv = 1[argv];
   }
@@ -32,15 +32,27 @@ int main(int argc, char const *argv[])
    
   serv_addr.sin_family = AF_INET; 
   serv_addr.sin_port = htons(PORT); 
-     
+  
   // Convert IPv4 and IPv6 addresses from text to binary form 
   if(inet_pton(AF_INET, ip_serv, &serv_addr.sin_addr)<=0) { 
     printf("\nInvalid address/ Address not supported \n"); 
     return -1; 
   } 
-   
+
+  address.sin_family = AF_INET; 
+  address.sin_addr.s_addr = INADDR_ANY; 
+  address.sin_port = htons(mPORT);
+  if (bind(sock, (struct sockaddr*) &address, sizeof(struct sockaddr_in)) == 0){
+    printf("Binded Correctly\n");
+  } else {
+    printf("Unable to bind\n");
+    close(sock);
+    return -1;
+  }
+
   if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) { 
     printf("\nConnection Failed \n"); 
+    close(sock);
     return -1; 
   }
 
@@ -57,6 +69,6 @@ int main(int argc, char const *argv[])
       printf("recived::\t\t%s\n", buffer);
     } while(scanf("%126[^\n]",buffer));
   }
-
+  close(sock);
   return 0; 
 } 
